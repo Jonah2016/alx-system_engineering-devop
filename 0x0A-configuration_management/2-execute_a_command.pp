c5ebf148fe4ad532f2438cc0567ecfd1ffaecf30
“Jonah <etuafuljonah@gmail.com>
2023-12-22 17:15:21 -0800

    update

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # kill process killmenow
 
-exec { 'kill_the_killmenow_process':
-  command => '/usr/bin/pkill -f killmenow',
+exec { 'pkill':
+  command  => 'pkill killmenow',
   provider => 'shell',
 }
-
