6d6db4e35c5f4ff7469dee071786a7cce75d59a0
“Jonah <etuafuljonah@gmail.com>
2023-12-22 18:03:22 -0800

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
+#!/usr/bin/pip3
 # Install an especific version of flask (2.1.0)
-
-package { 'python3-pip':
-  ensure => present,
-}
-
-package { 'Flask':
-  ensure          => '2.1.0',
-  provider        => 'pip3',
-  install_options => ['--upgrade'],
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
