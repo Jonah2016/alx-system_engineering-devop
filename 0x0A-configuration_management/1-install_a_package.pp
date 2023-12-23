99bcdbd3ad8aae5ed57bd895fdbd5fc06c480f01
“Jonah <etuafuljonah@gmail.com>
2023-12-22 17:11:50 -0800

    changes to 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
 
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+exec { 'kill_process_killmenow':
+  command  => '/usr/bin/pkill -f killmenow',
+  provider => 'shell',
 }
