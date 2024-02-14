9b42d1b24a828529555fb556c54aecf042b428b3
“Jonah <etuafuljonah@gmail.com>
2024-02-14 14:43:04 -0800

    full pi advanced codes

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Query for the amnt of users
+in a subreddit"""
+
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Function to retrieve the number of
+    subscribers of a given reddit
+
+    Parameters:
+        subreddit - subreddit to check.
+
+    Return:
+        Number of subscribers or 0 if not found
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+    headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+            AppleWebKit/537.36 (KHTML, like Gecko)\
+            Chrome/102.0.0.0 Safari/537.36'
+            }
+    r = requests.get(
+            f"https://www.reddit.com/r/{subreddit}/about.json",
+            allow_redirects=False,
+            headers=headers
+            )
+    if r.status_code != 200:
+        return 0
+    return (r.json()["data"]["subscribers"])
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Query for all of the hottest posts
+in a subreddit"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], params={}):
+    """ Function to retrieve the top 10 hottest
+    posts of a givne subreddit
+
+    Parameters:
+        subreddit - subreddit to check.
+        hot_list - list of hot titles.
+
+    Return:
+        PRINT titles of top 10 hottest posts, or None
+    """
+    headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+            AppleWebKit/537.36 (KHTML, like Gecko)\
+            Chrome/102.0.0.0 Safari/537.36'
+            }
+    r = requests.get(
+            f"https://www.reddit.com/r/{subreddit}/hot.json",
+            allow_redirects=False,
+            headers=headers,
+            params=params
+            )
+    if r.status_code != 200:
+        return
+    for child in r.json()["data"]["children"][:-1]:
+        hot_list.append(child["data"]["title"])
+    if r.json()["data"]["after"] is None:
+        return hot_list
+    after_value = r.json()["data"]["after"]
+    return recurse(subreddit, hot_list, {"after": after_value})
